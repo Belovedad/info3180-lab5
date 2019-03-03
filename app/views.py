a2bcbfaf1f799f076b6a3d78941d63cbec4c6fe0
Daniel Dormer <daniel.dormer@mymona.uwi.edu>
2019-03-03 20:58:31 +0000

    step 10

--- app/views.py
+++ app/views.py
@@ -22,6 +22,13 @@ from werkzeug.security import check_password_hash
 def secure_page():
     """Render Secure Page"""
     return render_template('secure_page.html')
+    
+@app.route("/logout")
+@login_required
+def logout():
+    logout_user()
+    flash('You have been logged out.', 'danger')
+    return redirect(url_for('home'))
 
 @app.route('/')
 def home():
@@ -59,8 +66,10 @@ def login():
                 login_user(user)
 
             # remember to flash a message to the user
-                flash('Logged in successfully.')
+                flash('Logged in successfully.', 'success')
                 return redirect(url_for("secure_page"))  # they should be redirected to a secure-page route instead
+            else: 
+                flash('Username or Password is incorrect.', 'danger')
     return render_template("login.html", form=form)
 
 
